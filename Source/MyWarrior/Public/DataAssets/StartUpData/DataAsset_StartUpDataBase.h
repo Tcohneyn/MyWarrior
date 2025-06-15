0292a80ff9aa1f6743d42e3c8ac294add3b9a2af
Tcohneyn <chenminami@outlook.com>
2025-06-16 01:58:06 +0800

    4.8-4.10设置好了敌我的属性以及Gameplay Effect。

--- Source/MyWarrior/Public/DataAssets/StartUpData/DataAsset_StartUpDataBase.h
+++ Source/MyWarrior/Public/DataAssets/StartUpData/DataAsset_StartUpDataBase.h
@@ -8,7 +8,7 @@
 
 class UWarriorGameplayAbility;
 class UWarriorAbilitySystemComponent;
-
+class UGameplayEffect;
 UCLASS()
 class MYWARRIOR_API UDataAsset_StartUpDataBase : public UDataAsset
 {
@@ -22,6 +22,9 @@ protected:
 
     UPROPERTY(EditDefaultsOnly, Category = "StartUpData")
     TArray<TSubclassOf<UWarriorGameplayAbility>> ReactiveAbilities;
+
+    UPROPERTY(EditDefaultsOnly, Category = "StartUpData")
+    TArray<TSubclassOf<UGameplayEffect>> StartUpGameplayEffects;
 
     void GrantAbilities(const TArray<TSubclassOf<UWarriorGameplayAbility>>& InAbilitiesToGive,
         UWarriorAbilitySystemComponent* InASCToGive, int32 ApplyLevel = 1);
